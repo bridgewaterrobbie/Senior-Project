dc189509ab72283bcf7fb6fb6374cbf986c7d24a
bridgewaterrobbie <bridgewaterrobbie@gmail.com>
2016-09-21 22:17:01 -0400

    Thomas Update leapclassifier

---
+++ Senior Project/Assets/Scripts/ThomasLeapTrainer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThomasLeapTrainer : MonoBehaviour {
+    /*
+	// Use this for initialization
+	void Start () {
+        LeapTrainer trainer = new LeapTrainer();
+
+        trainer.Create("Halt", false);
+        trainer.OnGestureCreated("Halt", {
+
+        });
+
+ 
+        trainer.OnGestureCreated()
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+}
