353ae759c271c544b8a338b13a37fea4c91f4a04
DESKTOP-KV4UJUU\Thomas Baldwin <thomasbaldwinj@gmail.com>
2016-09-28 22:18:47 -0400

    Leap Motion Fix

--- Senior Project/Assets/LeapMotion/Scripts/Utils/KeyEnableGameObjects.cs
+++ Senior Project/Assets/LeapMotion/Scripts/Utils/KeyEnableGameObjects.cs
@@ -2,24 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Leap.Unity{
-  public class KeyEnableGameObjects : MonoBehaviour {
-    public List<GameObject> targets;
-    [Header("Controls")]
-    public KeyCode unlockHold = KeyCode.RightShift;
-    public KeyCode toggle = KeyCode.T;
-  
-  	// Update is called once per frame
-  	void Update () {
-      if (unlockHold != KeyCode.None &&
-          !Input.GetKey (unlockHold)) {
-        return;
+public class KeyEnableGameObjects : MonoBehaviour {
+  public List<GameObject> targets;
+  [Header("Controls")]
+  public KeyCode unlockHold = KeyCode.RightShift;
+  public KeyCode toggle = KeyCode.T;
+
+	// Update is called once per frame
+	void Update () {
+    if (unlockHold != KeyCode.None &&
+        !Input.GetKey (unlockHold)) {
+      return;
+    }
+	  if (Input.GetKeyDown (toggle)) {
+      foreach (GameObject target in targets) {
+        target.SetActive(!target.activeSelf);
       }
-  	  if (Input.GetKeyDown (toggle)) {
-        foreach (GameObject target in targets) {
-          target.SetActive(!target.activeSelf);
-        }
-      }
-  	}
-  }
+    }
+	}
 }
