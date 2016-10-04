46da516e3fe6636358eed0a9f59680a0b97edbb9
Jason Owens <jasontowens94@gmail.com>
2016-10-03 23:08:27 -0400

    warpedSphereNo

--- Senior Project/Assets/LeapClasifier/TemplateMatcher.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-
-public interface TemplateMatcher  {
-
-	List<Point> process(List<Point> gesture);
-	float match (List<Point> gesture, List<Point> trainingGesture);
-
-}
