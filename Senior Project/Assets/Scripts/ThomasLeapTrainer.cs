624fa378522f037056004279105f7ce6791981e0
bridgewaterrobbie <bridgewaterrobbie@gmail.com>
2016-10-09 15:26:19 -0400

    Updated camera sync, working on getting leaptrainer working

--- Senior Project/Assets/Scripts/ThomasLeapTrainer.cs
+++ Senior Project/Assets/Scripts/ThomasLeapTrainer.cs
@@ -1,23 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ThomasLeapTrainer : MonoBehaviour {
-    /*
-	// Use this for initialization
-	void Start () {
-        LeapTrainer trainer = new LeapTrainer();
+public class ThomasLeapTrainer : MonoBehaviour
+{
+    private bool isTraining = false;
+    public LeapTrainer trainer;
 
-        trainer.Create("Halt", false);
-        trainer.OnGestureCreated("Halt", {
+    // Use this for initialization
+    void Start () {
+      //  if (trainer = null)
+            //trainer = new LeapTrainer();
 
-        });
-
- 
-        trainer.OnGestureCreated()
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}*/
+        if (Input.GetButtonDown("Fire3"))
+            startTraining();
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            string s = trainer.toJSON("TestGest");
+            Debug.Log(s);
+        }
+            
+        if (trainer != null)
+        {
+
+          //  Debug.Log("Trainer not null");
+            trainer.OnGestureRecognized += (name, value, allHits) => {
+                Debug.Log("Gesture recognized");
+            };
+         }
+        else
+        {
+           // Debug.Log("Trainer is null");
+        }
+    }
+
+    void startTraining()
+    {
+        if(isTraining)
+        {
+            return;
+        }
+        isTraining = true;
+        Debug.Log("Button Pressed");
+        if (trainer == null)
+            Debug.Log("Trainer is Null!");
+        else
+        {
+            trainer.Create("TestGest", false);
+            Debug.Log("Gesture Created");
+            
+        }
+        isTraining = false;
+    }
 }
